b48e8ebd580ae285fca2400e795092065e17cbdc
Akshithre <akshithallam@gmail.com>
2025-02-05 18:29:54 +0530

    second commit

--- src/components/Layout/Navigation.jsx
+++ src/components/Layout/Navigation.jsx
@@ -27,7 +27,7 @@ const Navigation = () => (
 
         <div>
           <button className="bg-red-600 hover:bg-red-700 text-white px-6 py-2 rounded-md">
-            DONATE NOW
+            DONATE
           </button>
         </div>
       </div>
